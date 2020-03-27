2f7e51e274a0ab294335f452e5013f12a2965fe4
root <root@DESKTOP-GBBE9KO.localdomain>
2020-03-27 12:27:28 +0100

    5

--- testscript.sh
+++ testscript.sh
@@ -4,4 +4,4 @@ NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 
 echo "this was run automagically"
-echo "da vidimo da li ce ga opali posle commita!!! hm.... pokusaj 5!!!"
+echo "da vidimo da li ce ga opali posle commita!!! hm.... pokusaj 6!!!"
